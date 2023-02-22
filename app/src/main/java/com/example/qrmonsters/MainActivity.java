f590d3ce49c55517bd828380f1ea0c546387c9ac
Nathan Bantug <josemari@ualberta.ca>
2023-02-21 23:05:23 -0700

    Removed_Firebase_Test_Data

--- app/src/main/java/com/example/qrmonsters/MainActivity.java
+++ app/src/main/java/com/example/qrmonsters/MainActivity.java
@@ -44,26 +44,5 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        // Create a new user with a first and last name
-        Map<String, Object> user = new HashMap<>();
-        user.put("first", "Ada");
-        user.put("last", "Lovelace");
-        user.put("born", 1815);
-
-// Add a new document with a generated ID
-        db.collection("users")
-                .add(user)
-                .addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
-                    @Override
-                    public void onSuccess(DocumentReference documentReference) {
-                        Log.d("Working", "DocumentSnapshot added with ID: " + documentReference.getId());
-                    }
-                })
-                .addOnFailureListener(new OnFailureListener() {
-                    @Override
-                    public void onFailure(@NonNull Exception e) {
-                        Log.w("Working", "Error adding document", e);
-                    }
-                });
     }
 }
