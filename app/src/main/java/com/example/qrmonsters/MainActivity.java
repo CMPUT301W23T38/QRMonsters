b86c3840d2a3c1ce31fa59f7b1d84ebe7b14a59c
Abdullah Mohammed <37706742+AbdullahJ1445@users.noreply.github.com>
2023-03-08 19:35:22 -0700

    mainactivityimport

--- app/src/main/java/com/example/qrmonsters/MainActivity.java
+++ app/src/main/java/com/example/qrmonsters/MainActivity.java
@@ -3,16 +3,19 @@ package com.example.qrmonsters;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.firebase.firestore.FirebaseFirestore;
 
 //import com.google.firebase.firestore.FirebaseFirestore;
 
 public class MainActivity extends AppCompatActivity {
 
 // <<<<<<< QRScanner_Integrate 
-FirebaseFirestore db = FirebaseFirestore.getInstance();
-Button btm;
+    FirebaseFirestore db = FirebaseFirestore.getInstance();
+    Button btm;
 // =======
 //     //FirebaseFirestore db = FirebaseFirestore.getInstance();
 // >>>>>>> main
