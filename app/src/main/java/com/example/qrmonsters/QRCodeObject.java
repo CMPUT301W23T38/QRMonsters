3961d54447cfffdf6cda7e1d799e259148156e71
Abdullah Mohammed <37706742+AbdullahJ1445@users.noreply.github.com>
2023-03-09 19:41:37 -0700

    Update QRCodeObject()

--- app/src/main/java/com/example/qrmonsters/QRCodeObject.java
+++ app/src/main/java/com/example/qrmonsters/QRCodeObject.java
@@ -1,6 +1,10 @@
 package com.example.qrmonsters;
 
 import android.location.Location;
+
+import androidx.annotation.NonNull;
+
+import java.util.HashMap;
 /**
 
  The QRCodeObject class represents a QR code object that contains information such as its name, hash, score, and location.
@@ -11,6 +15,9 @@ public class QRCodeObject {
     private String codeHash;
     private Integer codeScore;
     private Location codeLocation;
+    //Hashmap<string, string>  for qrcodeobject to represent comments
+    HashMap<String, String> comments = new HashMap<>();
+
     /**
 
      Constructs a QRCodeObject with the given name, hash, score, and location.
@@ -38,6 +45,11 @@ public class QRCodeObject {
         this.codeScore = codeScore;
         this.codeLocation = null;
     }
+
+    public QRCodeObject() {
+        // Required empty constructor for Firebase Database
+    }
+
     /**
 
      Returns the name of the QR code.
@@ -69,4 +81,53 @@ public class QRCodeObject {
     public Location getCodeLocation() {
         return codeLocation;
     }
+
+    /**
+     * Returns the comments of the QR code.
+     * @return the comments of the QR code
+     */
+    public HashMap<String, String> getComments() {
+        return comments;
+    }
+
+    /**
+     * Sets the comments of the QR code.
+     * @param comments the comments of the QR code
+     */
+    public void setComments(HashMap<String, String> comments) {
+        this.comments = comments;
+    }
+
+    /**
+     * Adds a comment to the QR code.
+     * @param comment the comment to add to the QR code
+     */
+    public void addComment(String comment) {
+        comments.put(comment, comment);
+    }
+
+    /**
+     * Removes a comment from the QR code.
+     * @param comment the comment to remove from the QR code
+     */
+    public void removeComment(String comment) {
+        comments.remove(comment);
+    }
+
+    /**
+     * Returns the string representation of the QR code.
+     * @return the string representation of the QR code
+     */
+    @NonNull
+    @Override
+    public String toString() {
+        return "QRCodeObject{" +
+                "codeName='" + codeName + '\'' +
+                ", codeHash='" + codeHash + '\'' +
+                ", codeScore=" + codeScore +
+                ", codeLocation=" + codeLocation +
+                ", comments=" + comments +
+                '}';
+    }
+
 }
